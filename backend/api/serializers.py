52deea0c5b7a6bf73c5ac94ac67084bd0bf88e35
Khityaev Kirill <119692441+KhitK0101@users.noreply.github.com>
2023-08-06 23:43:13 +0300

    addch api-mana/ingr

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
-                            ShoppingCart, Tag, User)
+                            ShoppingCart, Tag)
 from users.models import Subscription, User
 
 
