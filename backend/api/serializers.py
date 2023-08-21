b96af9c8a398a75e3c4e0a808de155c2f3bcb9f7
Khityaev Kirill <119692441+KhitK0101@users.noreply.github.com>
2023-08-21 09:08:08 +0300

    whw to_representation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -192,7 +192,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         queryset = Recipe.objects.filter(id=instance.id)
         recipe = get_object_or_404(queryset)
-    
+
         return RecipeFullSerializer(
             recipe, context={'request': self.context.get('request')}
         ).data
