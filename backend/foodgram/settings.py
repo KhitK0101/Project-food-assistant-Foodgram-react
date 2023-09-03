408ccb4e9c6caff8ea4c3c9754d7736bfd774f95
Khityaev Kirill <119692441+KhitK0101@users.noreply.github.com>
2023-09-03 13:40:06 +0300

    dock and sett

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -96,8 +96,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/collected_static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
